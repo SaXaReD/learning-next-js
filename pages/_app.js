677f32c11cd292215d35049d98c788923401082a
SaXaReD <saxared15@gmail.com>
2024-12-14 19:44:52 -0300

    first init

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import "@/styles/globals.css";
+import Head from "next/head";
+import Layout from "@/components/Layout";
+import Image from "next/image";
+import youtubeImg from "../public/youtube.png"
+import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => (
+  <Layout>
+    <Head>
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet" />
+    </Head>
+    <main>
+      <Component {...pageProps} />
+    </main>
+  </Layout>
+);
+
+export default App;
